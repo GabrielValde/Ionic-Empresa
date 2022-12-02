9a53f6fd2cae8433e05cf2696ae71bacee0d4d8f
GabrielValde <gabriel.valdez@pentcloud.com>
2022-12-01 23:20:23 -0600

    Agregue la diferencia de colores

--- src/app/components/button-menu/button-menu.component.ts
+++ src/app/components/button-menu/button-menu.component.ts
@@ -11,6 +11,8 @@ export class ButtonMenuComponent implements OnInit {
   @Input() Text:string="";
   @Input() Icon:string="";
   @Input() Link:string="";
+  Estilo:string[]=[];
+  @Input() TypeStyle:boolean=false;
   Colapsar:boolean=false;
   constructor(private ColapsarService:MenuService) {   
   }
@@ -22,6 +24,7 @@ export class ButtonMenuComponent implements OnInit {
     this.ColapsarService.observableColapsar.subscribe(
       valor=> this.Colapsar=valor
     )
+    this.Estilo= this.TypeStyle? ['bg-red-300','text-red-600','rounded-3xl'] : ['text-red-600','font-bold']
   }
 
 }
